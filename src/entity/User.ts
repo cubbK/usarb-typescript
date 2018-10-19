3e84a5d678fbb36f9651c8274a9e57fa5e2c2590
Dan <danpopa1998@gmail.com>
2018-10-19 11:10:22 +0300

    start adding comment

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,7 +1,28 @@
 import { Person } from "./Person";
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Comment } from "./Comment";
+import { Entity, PrimaryGeneratedColumn, Column, getManager } from "typeorm";
 
 @Entity()
 export class User extends Person {
-  
+
+    async getComments() {
+        const entityManager = getManager(); // you can also get it via getConnection().manager
+        const comments = await entityManager.find(Comment);
+        return comments;
+    }
+
+    async addComment(text) {
+        const entityManager = getManager();
+        const thisUser = await entityManager.findOne(User, { where: { id: this.id } })
+        const thisUserId = thisUser.id;
+        
+        const comment = await entityManager.create(Comment, {
+            text: text,
+            person: thisUserId
+        })
+        entityManager.save(comment);
+        console.log("added comment", comment);
+        return comment;
+    }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {Person} from "./entity/Person";
-import {User} from "./entity/User"
+import { createConnection } from "typeorm";
+import { Person } from "./entity/Person";
+import { User } from "./entity/User"
+import { Comment } from "./entity/Comment";
 
 createConnection().then(async connection => {
 
-    const user = new User();
-    user.username = "user1";
-    user.password = "secret1";
-    await connection.manager.save(user);
+    const user = await connection.manager.findOne(User, { where: { id: 1 } });
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded persons: ", users);
-     
-    console.log("Here you can setup and run express/koa/any other framework.");
+    const comments = await user.getComments();
+    console.log(user);
+
+    const newComment = await user.addComment("comment 1");
+
     
+    // console.log("Loading users from the database...");
+    // const users = await connection.manager.find(User);
+    // console.log("Loaded persons: ", users);
+
+    // console.log("Here you can setup and run express/koa/any other framework.");
+
 }).catch(error => console.log(error));
